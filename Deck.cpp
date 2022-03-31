c3004f0f5efabcfeccc48741afa9291cb26e23bd
Karina Hernandez <92545495+karypaquot@users.noreply.github.com>
2022-03-31 13:38:08 -0700

    Update Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -5,3 +5,14 @@ using namespace std;
 
 Deck::Deck() 
 {
+ char rank[] = {'A','2','3','4','5','6','7','8','9','T','J','Q','K'};
+  char suit[] = {'S','H','D','C'};
+  int size = 0;
+  for (int row = 0; row < 4; row++){
+    for(int col = 0; col < 13; col++ ){
+      deck[size].setCard(rank[col], suit[row]);
+      size++;
+    }
+  }
+  topCard = 0;
+} 
