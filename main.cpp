bb776c3dc9a62eb57a200e7c024ff4ef8ce497ee
Karina Hernandez <92545495+karypaquot@users.noreply.github.com>
2022-03-31 13:28:21 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,7 @@
+// Karina Hernandez
+// Class (CECS 282-01)
+// Project Name (Prog 1 – Solitaire Prime)
+// Due Date (9/13/21)
+// 
+// I certify that this program is my own original work. I did not copy any part of this program from 
+//any other source. I further certify that I typed each and every line of code in this program.
