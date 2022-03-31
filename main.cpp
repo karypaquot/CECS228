301f4c23d385ee72339c7c477315fe1d6d3f732b
Karina Hernandez <92545495+karypaquot@users.noreply.github.com>
2022-03-31 13:29:02 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,3 +5,10 @@
 // 
 // I certify that this program is my own original work. I did not copy any part of this program from 
 //any other source. I further certify that I typed each and every line of code in this program.
+
+#include <iostream>
+#include "Card.h"
+#include "Deck.h"
+#include <string>
+using namespace std;
+
