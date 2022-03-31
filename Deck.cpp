01edd79484c03b62e3355bc3b053e6da4a10bebe
Karina Hernandez <92545495+karypaquot@users.noreply.github.com>
2022-03-31 13:38:27 -0700

    Update Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -16,3 +16,17 @@ Deck::Deck()
   }
   topCard = 0;
 } 
+
+void Deck::refreshDeck()
+{
+  char rank[] = {'A','2','3','4','5','6','7','8','9','T','J','Q','K'};
+  char suit[] = {'S','H','D','C'};
+  int size = 0;
+  for (int row = 0; row < 4; row++){
+    for(int col = 0; col < 13; col++ ){
+      deck[size].setCard(rank[col], suit[row]);
+      size++;
+    }
+  }
+  topCard = 0;
+}
