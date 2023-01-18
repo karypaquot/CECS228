c7a910d7dfb477d5ca0269ce113833d380c1b19c
Karina Hernandez <92545495+karypaquot@users.noreply.github.com>
2023-01-17 16:49:27 -0800

    Create main.cpp

---
+++ Labs/Lab1/main.cpp
@@ -0,0 +1,15 @@
+// Karina Hernandez
+// CECS 282-03
+// Week 1, Lab 1
+// 8/23/2021
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string greeting = "Hello World!";
+  cout << greeting << endl;
+  cout << "I Love C++!\n";
+  return 0;
+}
